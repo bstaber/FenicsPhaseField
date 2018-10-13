58eeeefb37c46c1fdcf4e2cf045837b2a7dbbc2c
Brian <brian.staber@gmail.com>
2018-10-14 01:10:42 +0200

    Added parameters

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fenics import *
 import matplotlib.pyplot as plt
 
+#parameters
+parameters["linear_algebra_backend"] = "PETSc"
+prm = parameters["krylov_solver"]
+prm["absolute_tolerance"]  = 1E-10
+prm["relative_tolerance"]  = 1E-6
+prm["maximum_iterations"]  = 1000
+prm["monitor_convergence"] = False
+set_log_level(LogLevel.PROGRESS)
+
+info(parameters, True)
+
+#load mesh
 mesh = Mesh('meshes/mesh_fenics.xml')
