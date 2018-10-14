66e4a7c7a19831ae1a95a564c12386cfe4f69d29
Brian <brian.staber@gmail.com>
2018-10-14 10:57:15 +0200

    Added boundary conditions

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ prm["maximum_iterations"]  = 1000
 prm["monitor_convergence"] = False
 set_log_level(LogLevel.PROGRESS)
 
-info(parameters, True)
+info(parameters, False)
 
 #load mesh
 mesh = Mesh('meshes/mesh_fenics.xml')
@@ -19,6 +19,24 @@ W = VectorFunctionSpace(mesh=mesh, family='Lagrange', degree=1, dim=2)
 
 #define boundary conditions
 
+ud = Expression("t", t=0.0, degree=1)
+
+def bottom(x, on_boundary):
+    tol = 1E-8
+    return on_boundary and abs(x[1]) < tol
+
+def top(x, on_boundary):
+    tol = 1E-8
+    return on_boundary and abs(x[1]-1.0) < tol
+
+bottomBCs = DirichletBC(W, Constant((0.0, 0.0)), bottom)
+topBC_x   = DirichletBC(W.sub(0), ud, top)
+topBC_y   = DirichletBC(W.sub(1), Constant(0.0), top)
+
+bcs = [bottomBCs, topBC_x, topBC_y]
+
 #define variational problems
+
+
 
 #solver
