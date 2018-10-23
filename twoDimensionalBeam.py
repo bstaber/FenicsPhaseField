631bdac0f695ce5c12506d27f333c83f5daec562
Brian <brian.staber@gmail.com>
2018-10-23 09:09:08 +0200

    Testing alternative implementation for the 2D beam

--- twoDimensionalBeam.py
+++ twoDimensionalBeam.py
@@ -18,9 +18,11 @@ info(parameters, False)
 #----------------------------------------------------------------------#
 # Load mesh and define functional spaces
 mesh = Mesh('meshes/srb.xml')
+"""
 mesh = refine(mesh)
+"""
 V = FunctionSpace(mesh, 'Lagrange', 1)
-W = VectorFunctionSpace(mesh, 'Lagrange', 1, mesh.topoogy().dim())
+W = VectorFunctionSpace(mesh, 'Lagrange', 1, mesh.topology().dim())
 #----------------------------------------------------------------------#
 
 
@@ -43,12 +45,19 @@ def right(x, on_boundary):
     tol = 1E-8
     return on_boundary and abs(x[0]-1.0) < tol and between(x[1], (0.045, 0.055))
 
+
 def left(x, on_boundary):
     tol = 1E-8
     return on_boundary and abs(x[0]) < tol and between(x[1], (0.045, 0.055))
 
+def bottom(x, on_boundary):
+    tol = 1E-8
+    return on_boundary and abs(x[1]) < tol
+
+
 bcs = [DirichletBC(W, Constant((0.0, 0.0)), left),
-       DirichletBC(W.sub(0), ud, right)]
+       DirichletBC(W.sub(0), ud, right),
+       DirichletBC(W.sub(1), Constant(0.0), right)]
 #----------------------------------------------------------------------#
 
 
@@ -74,13 +83,44 @@ u, v = TrialFunction(W), TestFunction(W)
 uold, unew = Function(W), Function(W)
 dnew       = Function(V)
 
-histold_0  = energy_density_positive(uold, lmbda0, mu0)
-histnew_0  = energy_density_positive(unew, lmbda0, mu0)
-hist_0     = Max(histold_0, histnew_0)
+def energy_density_positive_bulk(u):
+    e     = epsilon(u)
+    tr_e  = tr(e)
+    return 0.5*conditional(gt(tr_e,0.0),tr_e*tr_e,0.0)
 
-histold_1  = energy_density_positive(uold, lmbda1, mu1)
-histnew_1  = energy_density_positive(unew, lmbda1, mu1)
-hist_1     = Max(histold_1, histnew_1)
+def energy_density_positive_shear(u):
+    e     = epsilon(u)
+    tr_e  = tr(e)
+    det_e = det(e)
+    disc  = sqrt(tr_e*tr_e/4.0 - det_e)
+    v1    = 0.5*tr_e + disc
+    v2    = 0.5*tr_e - disc
+    v1p   = conditional(gt(v1,0.0),v1,0.0)
+    v2p   = conditional(gt(v2,0.0),v2,0.0)
+    return v1p*v1p + v2p*v2p
+
+"""
+energyold_bulk = energy_density_positive_bulk(uold)
+energynew_bulk = energy_density_positive_bulk(unew)
+
+energyold_shear = energy_density_positive_shear(uold)
+energynew_shear = energy_density_positive_shear(unew)
+
+histold_0 = lmbda0*energyold_bulk + mu0*energyold_shear
+histnew_0 = lmbda0*energynew_bulk + mu0*energynew_shear
+
+histold_1 = lmbda1*energyold_bulk + mu1*energyold_shear
+histnew_1 = lmbda1*energynew_bulk + mu1*energynew_shear
+
+hist_0 = Max(histold_0, histnew_0)
+hist_1 = Max(histold_1, histnew_1)
+"""
+
+histold_0, histnew_0, hist_0 = Function(V), Function(V), Function(V)
+histold_1, histnew_1, hist_1 = Function(V), Function(V), Function(V)
+
+hist_0 = Max(histold_0, histnew_0)
+hist_1 = Max(histold_1, histnew_1)
 
 Id = ((2.0*hist_0 + gc0/lc)*dot(d,s) + gc0*lc*inner(nabla_grad(d), nabla_grad(s)) - 2.0*hist_0*s)*dx(0) \
    + ((2.0*hist_1 + gc1/lc)*dot(d,s) + gc1*lc*inner(nabla_grad(d), nabla_grad(s)) - 2.0*hist_1*s)*dx(1)
@@ -103,29 +143,39 @@ solver_disp = LinearVariationalSolver(prob_disp)
 
 #----------------------------------------------------------------------#
 # Staggered algorithm
-nsteps = 1000
-delta  = 1E-4
+nsteps = 100
+delta  = 1E-3
 
 vtkfile_d = File('results/2d_beam_deterministic/damagefield.pvd')
 
+hist_0.vector().zero()
+hist_1.vector().zero()
+d.vector().zero()
+
 for n in range(nsteps):
+
     ud.t += delta
 
-    dnew.assign(d)
+    if (n>0):
+        solver_dmge.solve()
+        dnew.assign(d)
+
     uold.assign(u)
-
     solver_disp.solve()
-
     unew.assign(u)
 
-    solver_dmge.solve()
+    energynew_bulk  = energy_density_positive_bulk(unew)
+    energynew_shear = energy_density_positive_shear(unew)
 
+    histnew_0 = lmbda0*energynew_bulk + mu0*energynew_shear
+    histnew_1 = lmbda1*energynew_bulk + mu1*energynew_shear
+
+    histold_0.assign(hist_0)
+    histold_1.assign(hist_1)
+
+    plot(d, cmap='jet')
+    plt.draw()
+    plt.pause(0.0001)
+    """
     vtkfile_d << (d, n+1)
-
-
-plot(d, cmap='jet', range_min=0., range_max=1.)
-plt.show()
-"""
-plt.title('Iteration {}'.format(n))
-plt.pause(0.0001)
-"""
+    """
