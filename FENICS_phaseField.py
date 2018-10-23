e430221b79d0a32e4009b9948395c98152f2922c
Brian <brian.staber@gmail.com>
2018-10-23 09:46:24 +0200

    Removed one file

--- FENICS_phaseField.py
+++
@@ -1,9 +0,0 @@
-from fenics import *
-
-class fullDegradation:
-
-    def __init__(self, lame, loading, mesh):
-        self.lmbda  = lame[0]
-        self.mu     = lame[1]
-        self.nsteps = loading[0]
-        self.delta  = loading[1]
